8c63b50c50f1897b36f98efad26f5d23430d2ccf
Lupita <lupitarivera8899@gmail.com>
2021-02-05 21:26:15 -0600

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,31 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :hero => {
+        #your key/value pairs here
+        },
+      :hero_friends => []
       #your key/value pairs here!
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+      }
+    }
 
   
 end
